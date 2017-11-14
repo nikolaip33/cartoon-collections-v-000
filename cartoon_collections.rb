7ea6a00d27920554a8aed279065ef002fcb4bb05
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-14 03:28:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,7 @@ def long_planeteer_calls(words)
   words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese(foods)
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.find do |food|
-    cheese_types.include?(food)
-  end
+  array.detect { |food| cheese_types.include?(food) }
 end
